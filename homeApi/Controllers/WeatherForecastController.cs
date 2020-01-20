56a7346a57b4fb2e9f6d4d9f3eea87dcbcf78c68
Jaroslaw J. G. Granat <jaroslaw.granat@tpvaoc.com>
2020-01-20 21:46:20 +0100

    test g1

--- homeApi/Controllers/WeatherForecastController.cs
+++ homeApi/Controllers/WeatherForecastController.cs
@@ -14,6 +14,7 @@ namespace homeApi.Controllers
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            // test git
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
